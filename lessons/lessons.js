897c959a8bd34c9100a617a3f455790222cb27d4
Ganzorig <ganzorig2022@iMac-18s-iMac.local>
2022-07-21 11:58:54 +0800

    lesson 4

--- lessons/lessons.js
+++ lessons/lessons.js
@@ -84,8 +84,6 @@
 
 // console.log(`Server runnning at http://localhost:${PORT}/`)
 
-
-
 // ===============================lesson-3========================
 ////////////////////////////////NPM MODULE////////////////////////
 
@@ -93,3 +91,96 @@
 //node -help
 //node argv.js -t "hi args"
 //"hiA args"
+
+// const argumentArr = process.argv
+// // console.log(arg.pop())
+// const secondEl = argumentArr[2]
+// const a = argumentArr[argumentArr.length - 1]
+// console.log(a)
+
+// if (secondEl === '-t') {
+//   console.log(argumentArr.slice(-1))
+// } else {
+//   console.log('Hello Arguments')
+// }
+
+////////////////////////////////CHALK MODULE////////////////////////
+// const chalk = require('chalk')
+
+// let bgColor = '',
+//   color = '',
+//   text = 'hellll'
+
+// let args = process.argv.slice(2)
+// let str = args.join('')
+
+// if (str.includes('-bg')) {
+//   let tmp = str.split('-bg')[1].split('-')[0]
+//   bgColor = 'bg' + tmp[0].toUpperCase() + tmp.slice(1).toLowerCase()
+// }
+// if (str.includes('-c')) {
+//   color = str.split('-c')[1].split('-')[0]
+// }
+// if (str.includes('-t')) {
+//   text = str.split('-t')[1].split('-')[0]
+// }
+// console.log(text, color, bgColor)
+// Shar medee-cblack-bgMagenta black-bgMagenta bgMagenta
+// Shar medee black bgMagenta
+// console.log(chalk[color][bgColor](text));
+
+///////////////////////////////////ODD EVEN NUMBER/////////////////
+// const readline = require('readline')
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// })
+// let odd = 0,
+//   even = 0
+
+// rl.question('ta utgaa oruulna uu', (input) => {
+//   let split = input.split(' ')
+//   even = split
+//     .map(Number)
+//     .filter((num) => num % 2 === 0)
+//     .reduce((num1, num2) => num1 * num2)
+
+//   odd = split
+//     .map(Number)
+//     .filter((num) => num % 2 === 1)
+//     .reduce((num1, num2) => num1 * num2)
+//   console.log('hariu is: ', even, odd)
+
+//   rl.close()
+// })
+
+///////////////FUNDAMENTALS of FUNCTIONAL PROGRAMMING
+
+//function stamement
+function sum1(x, y) {
+  return x + y
+}
+console.log(sum1(5, 2))
+
+//function expression
+const sum2 = (x, y) => {
+  return x + y
+}
+
+console.log(sum2(5, 2))
+
+//function arrow
+
+const sayMyName = (name) => 'hello ' + name
+
+console.log(sayMyName('Ganzo'))
+
+const double = (num) => num * 2
+
+console.log(double(2))
+
+const getAccess = (bool) => bool ? true : false
+
+console.log(getAccess(true));
+console.log(getAccess(false));
